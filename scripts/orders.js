c9bc20ab003af4a35fdc71b72742c660df14200a
FacundoGreco <facundogrecor@gmail.com>
2021-06-10 11:44:38 -0300

    Implements delete combo button

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,13 +1,36 @@
-function addNewCombo() {
-    
-    let btnAddCombo = document.getElementById('addCombo');
-    let combo = document.getElementById('combo');
-    let newCombo = combo.cloneNode(true);
+const combo = document.getElementById('combo');
 
+let comboCounter = 1;
+let comboIdNumber = 0;
+
+function addNewCombo() {
+
+    const btnAddCombo = document.getElementById('addCombo');
+    const newComboId = "combo" + (comboIdNumber + 1);
+
+    let newCombo = combo.cloneNode(true);
+    newCombo.id = newComboId;
     btnAddCombo.before(newCombo);
+
+    let deleteButtons = document.getElementsByClassName("delete");
+    deleteButtons[deleteButtons.length-1].setAttribute("onclick", "deleteCombo('"+newComboId+"');");
+
+    comboIdNumber++;
+    comboCounter++;
 }
 
+function deleteCombo(comboId) {
 
+    if (comboCounter > 1) {
+
+        const orderArea = document.getElementById("orderArea");
+        const combo = document.getElementById(comboId);
+        
+        orderArea.removeChild(combo);
+        comboCounter--;
+    }
+
+}
 
 function setDeliveryDisabled(disabled) {
 
