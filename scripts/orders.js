f5c5b4ada41a0a2c528fdf6893afb9f831f34fd5
FacundoGreco <facundogrecor@gmail.com>
2021-07-01 12:25:04 -0300

    Opens and closes cart

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,4 +1,23 @@
-/* LOAD PRODUCTS MENU */
+/* GLOBAL VARIABLES  */
+const cartSectionNode = document.querySelector('.cartSection');
+const cartCloseButton = document.querySelector('.cartCloseButton');
+
+/* FILLS MENU WITH PRODUCTS  */
 products.forEach(product => {
     product.appendProduct();
 });
+
+/* FILLS CART WITH ITEMS */
+
+/* OPEN CART */
+function openCart() {
+    cartSectionNode.classList = `cartSection cartSectionOpened`;
+}
+/* CLOSE CART */
+function closeCart(e) {
+    if ((e.target == cartSectionNode) || (e.target == cartCloseButton)) {
+        
+        cartSectionNode.classList = `cartSection cartSectionClosed`;
+
+    }
+}
